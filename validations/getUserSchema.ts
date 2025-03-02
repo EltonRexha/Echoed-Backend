5e0fad91679c5b63b72eadc7b84fcfc1226bbdc4
Elton Rexha <elton.rexha123@outlook.com>
2025-03-02 20:32:29 +0100

    fix request query bug

--- validations/getUserSchema.ts
+++ validations/getUserSchema.ts
@@ -1,15 +1,21 @@
 import { z } from 'zod';
 
 const getUserSchema = z.object({
-  username: z.string().optional(),
+  username: z.string().min(3).optional(),
   email: z.string().email().optional(),
   id: z.string().optional(),
-  page: z.string().refine((value) => !isNaN(Number(value)), {
-    message: 'skip must be a numeric string',
-  }).optional(),
-  limit: z.string().refine((value) => !isNaN(Number(value)), {
-    message: 'skip must be a numeric string',
-  }).optional(),
+  page: z
+    .string()
+    .refine((value) => !isNaN(Number(value)), {
+      message: 'skip must be a numeric string',
+    })
+    .optional(),
+  limit: z
+    .string()
+    .refine((value) => !isNaN(Number(value)), {
+      message: 'skip must be a numeric string',
+    })
+    .optional(),
 });
 
 export default getUserSchema;
