3dfce120a60dc15c50843c2b9e31959f41b94121
Elton Rexha <elton.rexha123@outlook.com>
2025-03-17 23:31:03 +0100

    Add route/controller to upload videos

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -11,7 +11,7 @@ import { unlink } from 'fs/promises';
 import path from 'path';
 import notFoundError from '../errors/errorTypes/notFoundError';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
-import { imageUpload } from '../config/multer';
+import { imageUpload, videoUpload } from '../config/multer';
 
 export const createPost = asyncHandler(async function (
   req: Request,
@@ -93,7 +93,10 @@ export const uploadPostImage = [
       },
     });
 
-    if (post?.Media && post.Media.length >= MAX_MEDIA_UPLOAD) {
+    if (
+      post?.Media &&
+      post.Media.length + (req.files.length as number) >= MAX_MEDIA_UPLOAD
+    ) {
       next(
         badRequestError(
           'Maximum amount of resources uploaded to post exceded',
@@ -146,6 +149,91 @@ export const uploadPostImage = [
 
     res.status(200).json({
       message: 'successfully uploaded image',
+    });
+  }),
+];
+
+export const uploadPostVideo = [
+  videoUpload.array('videos', MAX_MEDIA_UPLOAD),
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user as User;
+    if (!req.files && !Array.isArray(req.files)) {
+      next(badRequestError('No files provided'));
+      return;
+    }
+
+    const { postId } = req.params;
+
+    if (!postId) {
+      next(badRequestError('Post is required'));
+      return;
+    }
+
+    const post = await prisma.post.findUnique({
+      where: {
+        id: postId,
+      },
+      include: {
+        Media: true,
+      },
+    });
+
+    if (
+      post?.Media &&
+      post.Media.length + (req.files.length as number) >= MAX_MEDIA_UPLOAD
+    ) {
+      next(
+        badRequestError(
+          'Maximum amount of resources uploaded to post exceded',
+          'MAX_MEDIA'
+        )
+      );
+      return;
+    }
+
+    const files = req.files as Express.Multer.File[];
+
+    await Promise.all(
+      files.map(async (file) => {
+        const filePath = file.path;
+        const cloudinaryPath = `uploads/users/${user.id}/posts/${postId}/videos`;
+
+        try {
+          await prisma.post.update({
+            where: {
+              id: postId as string,
+            },
+            data: {
+              Media: {
+                create: {
+                  byteSize: file.size,
+                  mimeType: file.mimetype,
+                  path: cloudinaryPath,
+                },
+              },
+            },
+          });
+
+          const storedFile = await readFile(filePath);
+          await uploadStreamToCloudinary(storedFile, {
+            folder: cloudinaryPath,
+            resource_type: 'video',
+            public_id: `file_${Date.now()}`,
+          });
+        } catch (e) {
+          if (e instanceof PrismaClientKnownRequestError) {
+            throw notFoundError('Post not found');
+          } else {
+            throw e;
+          }
+        } finally {
+          await unlink(filePath);
+        }
+      })
+    );
+
+    res.status(200).json({
+      message: 'successfully uploaded video',
     });
   }),
 ];
--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import localUserAuthenticated from '../middlewares/localUserAuthenticated';
-import { createPost, uploadPostImage } from '../controllers/postController';
+import { createPost, uploadPostImage, uploadPostVideo } from '../controllers/postController';
 
 const router = Router();
 
 router.post('/', localUserAuthenticated, createPost);
-router.post('/:postId/image', localUserAuthenticated, uploadPostImage);
+router.post('/:postId/images', localUserAuthenticated, uploadPostImage);
+router.post('/:postId/videos', localUserAuthenticated, uploadPostVideo);
 
 export default router;
